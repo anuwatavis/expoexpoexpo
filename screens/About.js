dded92e2b636667dc5b9e29524819164dfe7e70f
anuwataravis <anuwat.avis@gmail.com>
2020-07-31 18:19:38 +0700

    add component

---
+++ screens/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const About = () => {
+  return (
+    <View style={styles.container}>
+      <Text>About Screens</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
+export default About;
---
+++ screens/ReviewDetail.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ReviewDetail = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={style.container}>ReviewDetail Screens</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
+
+export default ReviewDetail;
